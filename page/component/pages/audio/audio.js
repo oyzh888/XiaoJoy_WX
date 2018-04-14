955957d4dc48e1b9f5eb2c07e745dbfda5fc448a
oyzh888 <4d:c2:b4:74:55:73:24:77:43:b3:d0:18:82:e8:7d:8f>
2018-04-14 15:21:23 +0800

    first commit

---
+++ page/component/pages/audio/audio.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    current: {
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻',
+      author: '许巍',
+      src: 'https://xiaojoy.blob.core.chinacloudapi.cn/audios/%E6%9E%97%E4%BF%8A%E6%9D%B0%20-%20%E5%A5%B9%E8%AF%B4.mp3',
+    },
+    audioAction: {
+      method: 'pause'
+    }
+  }
+})
