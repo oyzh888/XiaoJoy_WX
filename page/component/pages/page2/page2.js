0e36ed3a8b5775b39b5db0a56c1502b476197ba6
liujiaheng <buaaljiaheng@gmail.com>
2018-04-15 09:13:50 +0800

    ljh's change

--- page/component/pages/page2/page2.js
+++ page/component/pages/page2/page2.js
@@ -1,17 +1,35 @@
 Page({
   data: {
-    current: {
-      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-      name: '此时此刻',
-      author: '许巍',
-      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    current1: {
+      poster: 'https://xiaojoy.blob.core.chinacloudapi.cn/audios/Joy.png',
+      name: 'Melody-3',
+      author: 'XiaoJoy',
+      src: 'https://xiaojoy.blob.core.chinacloudapi.cn/audios/2_AI_Single_LittleStar.mp3',
     },
+    current2: {
+      poster: 'https://xiaojoy.blob.core.chinacloudapi.cn/audios/xiao.png',
+      name: 'Melody-4',
+      author: 'XiaoJoy',
+      src: 'https://xiaojoy.blob.core.chinacloudapi.cn/audios/2_BachWTC1222.mp3',
+    },
+
     audioAction: {
       method: 'pause'
-    }
+    },
+
+  },
+  play1: function () {
+    this.audioCtx = wx.createAudioContext('audio1'),
+      this.audioCtx.pause()
+  },
+  play2: function () {
+    this.audioCtx = wx.createAudioContext('audio2'),
+      this.audioCtx.pause()
   },
   choose: function (b) {
     console.log('click');
+    this.play1();
+    this.play2();
     wx.showModal({
       title: 'Are you sure?',
       // content: '这是一个模态弹窗',
